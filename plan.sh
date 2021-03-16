5d9d2d0d8ac3e42f58ad26795b19079ee3e8ed91
Luu <lucas.giancola@live.com>
2021-03-16 11:32:05 -0400

    Bumped version 2.4.2 -> 2.5.4

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_name=libassuan
 pkg_origin=core
-pkg_version=2.4.2
+pkg_version=2.5.4
 pkg_license=('LGPL-2.0-or-later')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="ftp://ftp.gnupg.org/gcrypt/${pkg_name}/${pkg_name}-${pkg_version}.tar.bz2"
 pkg_description="Libassuan is a small library implementing the so-called Assuan protocol. "
 pkg_upstream_url=https://www.gnupg.org/software/libassuan/index.html
-pkg_shasum=bb06dc81380b74bf1b64d5849be5c0409a336f3b4c45f20ac688e86d1b5bcb20
+pkg_shasum=c080ee96b3bd519edd696cfcebdecf19a3952189178db9887be713ccbcb5fbf0
 pkg_deps=(core/glibc core/libgpg-error)
 pkg_build_deps=(core/gcc core/coreutils core/sed core/bison core/flex core/grep core/bash core/gawk core/libtool core/diffutils core/findutils core/xz core/gettext core/gzip core/make core/patch core/texinfo core/util-linux)
 pkg_bin_dirs=(bin)
